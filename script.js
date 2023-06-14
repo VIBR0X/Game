4c72cd932c209b0de21efdfa4b4573dcc240c48d
VIBR0X <44766005+vedant-github@users.noreply.github.com>
2023-06-14 23:27:06 +0530

    fix css fadein-out

--- script.js
+++ script.js
@@ -9,14 +9,11 @@ var bg_url = ['./assets/images/tomas-honz-act1-finaldev.jpg',
 const element = document.getElementById("hint-content"); 
 var i = 0;
 function changeText() {
-    element.classList.add("fade-out");
     element.textContent = hint_text[i];
     document.body.style.backgroundImage = `url(${bg_url[i]})`;
     setTimeout(() => {
         i = (i + 1) % hint_text.length;
-        element.classList.remove("fade-out")
-        element.classList.add("fade-in")
-        document.body.style.backgroundImage = `url(${bg_url[i]})`;
+        changeText()
     }, 5000);
 }
 
